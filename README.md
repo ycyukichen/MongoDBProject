6c98d3779fb28dea8b5b8704295b093b107a4eb7
Yuki Chen <yc0688a@american.edu>
2024-09-03 16:39:11 -0400

    new updates

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MangoDBProject
+# MongoDBProject
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+Hyphen_e = '-e .'
+def get_requirements(filename: str) -> List[str]:
+    requirements = []
+    with open(filename) as obj:
+        requirements = obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if Hyphen_e in requirements:
+            requirements.remove(Hyphen_e)
+    
+    return requirements
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+    
+setup(
+    name="MongoDBProject",
+    version="0.1.0",
+    author="Yu-Yu",
+    author_email="ycyukichen@gmail.com",
+    long_description="MongoDB Connect related operations",
+    url=f'https://github.com/ycyukichen/MongoDBProject.git',
+    install_requires=get_requirements("requirements.txt"),
+    package_dir={"": "src"},
+    packages=find_packages(where="src")
+)
